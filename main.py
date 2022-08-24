52a419b385b6469433b3844ab49c7cfb85f51253
Kamati-karl <90451534+Kamati-karl@users.noreply.github.com>
2022-08-24 10:51:37 +0200

    updated

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 import pandas
 
 # read the casv into pandas dataframe
-file = pandas.read_csv("test_data - test_data.csv")
+file = pandas.read_csv("test_data.csv")
+file["insight"] = ""
 
-#i9terate through rows of file
+#iterate through rows of file
 for index, row in file.iterrows():
+
+
 #convert text column data to lower in prep for comparison
     text = row["text"].lower()
+
 #check for text in rows for client
-    if row["role"] == "client":
+    if row["role"] == "manager":
+
         #if greetings found ibn row text mark by printint(note that pritning is temporary
         if "алло" in text or "здравствуйте" in text or "добрый день" in text:
-            print(f"greeting ","dialogue number: ", row["dlg_id"], "line number: ",row["line_n"],"role: ", row["role"])
+            file.insight[index] = "greetings = True"
+
         #if name indicator is in row print
         if "зовут" in text:
-            print( print(f"name ","dialogue number: ", row["dlg_id"], "line number: ",row["line_n"],"role: ", row["role"]))
+            file.insight[index] = "name mention = True"
+
         # if company indicator is in row print
         if "компания" in text:
-            print( print( print(f"company ","dialogue number: ", row["dlg_id"], "line number: ",row["line_n"],"role: ", row["role"])))
+            file.insight[index] = "company mention = True"
+
         #if goodbye is in row text print
         if "до свидания" in text or "всего доброго" in text:
-            print(f"goodbye ", "dialogue number: ", row["dlg_id"], "line number: ", row["line_n"], "role: ",
-                  row["role"])
+            file.insight[index] = "Goodbye = True"
 
+file.to_csv("test_data.csv", sep=',', index=False)
 
 
 
